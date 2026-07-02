5131b1e8be5352566bfa3e0773244cf4df035d3a
Ryuya0708 <yama150708@gmail.com>
2026-07-03 01:06:32 +0900

    finish done 001

--- apps/001-calculator/main.py
+++ apps/001-calculator/main.py
@@ -3,13 +3,13 @@ import tkinter as tk
 # create window
 root = tk.Tk()
 root.title("calculator")
-root.geometry("380x450")
+root.geometry("280x380")
 root.resizable(False, False)
 
 # display
 display = tk.Entry(
     root,
-    font=("Arial", 24),
+    font=("Arial", 20),
     width=16,
     justify="right",
 )
@@ -25,22 +25,26 @@ display.grid(
 
 # buttons
 buttons = [
+    "←",
+    "C",
+    "%",
+    "÷",
     "7",
     "8",
     "9",
-    "/",
+    "x",
     "4",
     "5",
     "6",
-    "*",
+    "-",
     "1",
     "2",
     "3",
-    "-",
+    "+",
+    "☺",
     "0",
     ".",
     "=",
-    "+",
 ]
 
 for i, button in enumerate(buttons):
@@ -49,9 +53,10 @@ for i, button in enumerate(buttons):
     tk.Button(
         root,
         text=button,
-        font=("Arial", 18),
+        font=("Arial", 12),
         width=5,
         height=2,
+        command=lambda b=button: click_event(b),
     ).grid(
         row=row + 1,
         column=column,
@@ -59,6 +64,65 @@ for i, button in enumerate(buttons):
         pady=5,
         sticky="nsew",
     )
+
+
+def click(value):
+    num = display.get()
+    new_text = num + value
+    display.delete(0, tk.END)
+    display.insert(0, new_text)
+
+    # display.insert(tk.END, value)でも書ける。練習で流れにそって書いた
+
+
+# 各項目の動作
+def clear():
+    display.delete(0, tk.END)
+
+
+def calculate():
+    num = display.get()
+    result = eval(num)
+    display.delete(0, tk.END)
+    display.insert(0, result)
+
+
+def operator(value):
+    if value == "÷":
+        value = "/"
+    elif value == "x":
+        value = "*"
+    num = display.get()
+    new_text = num + value
+    display.delete(0, tk.END)
+    display.insert(0, new_text)
+
+
+def nikochan():
+    return None
+
+
+def backspace():
+    num = display.get()
+    new_text = num[:-1]
+    display.delete(0, tk.END)
+    display.insert(0, new_text)
+
+
+def click_event(button):
+    if button == "=":
+        calculate()
+    elif button == "C":
+        clear()
+    elif button in ["÷", "x", "-", "+"]:
+        operator(button)
+    elif button == "☺":
+        nikochan()
+    elif button == "←":
+        backspace()
+    else:
+        click(button)
+
 
 # start the event loop
 root.mainloop()
